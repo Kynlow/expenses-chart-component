c45aa32804ab787de0ca97153ae10497677a0329
Kynlow <elwen@areastudio.fr>
2026-02-23 17:12:44 +0100

    feat: add full system of delete task

--- app.js
+++ app.js
@@ -2,17 +2,38 @@ const $btn = document.querySelector("button");
 const $input = document.querySelector("#input1");
 const $container = document.querySelector("#itemContainer");
 const $h2 = document.querySelector("h2");
+const $label = document.querySelector("label");
+const $main = document.querySelector("main");
+const $form = document.querySelector("form");
+
+let numberOfChild = 0;
 
 function createDiv(content) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("item");
-    newDiv.textContent = content
-    $container.appendChild(newDiv)
+	const newDiv = document.createElement("div");
+	newDiv.classList.add("item");
+	newDiv.textContent = content;
+
+	const trash = document.createElement("button");
+	trash.classList.add("trash");
+	newDiv.appendChild(trash);
+	$container.appendChild(newDiv);
+
+    numberOfChild++;
+
+	trash.addEventListener("click", () => {
+		newDiv.remove();
+        numberOfChild--;
+		if (numberOfChild == 0) {
+			$main.appendChild($h2);
+		} else {
+			$h2.remove();
+		}
+	});
 }
 
-$btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    createDiv($input.value);
-    $input.value = ""
-    $h2.remove()
-})
+$form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	$h2.remove();
+	createDiv($input.value);
+	$form.reset();
+});
